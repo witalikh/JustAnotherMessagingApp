d4abf02ae05b36d479bd6257ee0019bbcf665b30
Vitalii <witalikhomyn@gmail.com>
2022-10-22 12:11:31 +0300

    Remove bad solution

--- ApplicationSolution/BusinessLogicLayer/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BusinessLogicLayer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
